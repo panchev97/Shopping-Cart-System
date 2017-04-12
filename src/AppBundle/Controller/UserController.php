9be48e79b6e7461e7afc511cdd18a0cffee13a40
panchev97 <rpanchev9708@gmail.com>
2017-04-12 20:45:01 +0300

    Login/Registration forms updated. Added Remember me functionality and users can login with emails

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -33,10 +33,53 @@ class UserController extends Controller
             $em->persist($user);
             $em->flush();
 
-           return $this->redirectToRoute('our_login');
+           return $this->redirectToRoute('products');
         }
         return $this->render('user/register.html.twig', array(
             'registerForm' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/user/{id}", name="user_profile")
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editProfileAction($id, Request $request)
+    {
+
+        $user = $this->getDoctrine()
+                     ->getRepository('AppBundle:User')
+                     ->find($id);
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //Get Data..
+            $username = $form['username']->getData();
+            $name = $form['name']->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $user = $em->getRepository('AppBundle:User')
+                        ->find($id);
+
+            $password = $this->get('security.password_encoder')
+                             ->encodePassword($user, $user->getPassword());
+
+            $user->setUsername($username);
+            $user->setName($name);
+            $user->setPassword($password);
+
+            $em->flush();
+
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('user/profile.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 }
--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -15,6 +16,7 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('email', EmailType::class)
             ->add('username', TextType::class)
             ->add('name', TextType::class)
             ->add('password', RepeatedType::class, array(
